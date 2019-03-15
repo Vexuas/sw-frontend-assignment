3d5f8cc58b258eb9a74122a15bd95375e6ff12c0
Gabe R <rodriguezgab1601@gmail.com>
2019-03-15 18:14:37 +0800

    Updates

---
+++ precache-manifest.732c544d414013fe240e21677bff5cfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "feface72008e6857da27f939e1eceffa",
+    "url": "/swTest-TeamPage/static/media/Proxima_Nova.feface72.ttf"
+  },
+  {
+    "revision": "ab8af94dad06f6183746",
+    "url": "/swTest-TeamPage/static/js/runtime~main.3d18a1ab.js"
+  },
+  {
+    "revision": "46ab137a7930af0e0ada",
+    "url": "/swTest-TeamPage/static/js/main.1fb0d603.chunk.js"
+  },
+  {
+    "revision": "dc012ac24166e9ed258a",
+    "url": "/swTest-TeamPage/static/js/2.0d258afd.chunk.js"
+  },
+  {
+    "revision": "46ab137a7930af0e0ada",
+    "url": "/swTest-TeamPage/static/css/main.9c310410.chunk.css"
+  },
+  {
+    "revision": "c003efcf475c0731f9716b2dc9559f21",
+    "url": "/swTest-TeamPage/index.html"
+  }
+];
